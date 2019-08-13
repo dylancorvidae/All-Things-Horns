d0e131876de656da0a6cdd31c0993640a95cc3d4
Joe Klause <joeklause@gmail.com>
2019-08-13 12:59:33 -0700

    fixed hover/passing travis

--- src/app.js
+++ src/app.js
@@ -9,12 +9,3 @@ images.forEach(animal => {
     const dom = htmlToDom(html);
     animalList.appendChild(dom);
 });
-
-const animalCard = document.getElementsByClassName('animal-card');
-
-[...animalCard].forEach((card) => {
-    card.addEventListener('mouseover', () => {
-        const description = document.getElementById('description');
-        description.classList.remove('hidden');
-    });
-});
